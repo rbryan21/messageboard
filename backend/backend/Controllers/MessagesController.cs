7f4acc06a6c14144792b5f20b4af90a1416af943
Robert Bryan <rbryan21@gmail.com>
2017-08-18 00:13:37 -0400

    Add backend, message model, hardcoded message data

---
+++ backend/backend/Controllers/MessagesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Messages")]
+    public class MessagesController : Controller
+    {
+        public  IEnumerable<Models.Message> Get()
+        {
+            return new Models.Message[] {
+                new Models.Message {
+                    Owner = "John",
+                    Text = "hello"
+                },
+                new Models.Message {
+                    Owner = "Tim",
+                    Text = "Hi"
+                }
+            };
+        }
+    }
+}
